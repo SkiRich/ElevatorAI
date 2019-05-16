2fabc300df32b543fb059bf9469702688a173e7a
SkiRich <richard@szkodzinski.com>
2019-05-16 01:45:41 -0400

    Upload to Steam

--- metadata.lua
+++ metadata.lua
@@ -1,8 +1,8 @@
 return PlaceObj('ModDef', {
 	'title', "Elevator A.I.",
-	'description', "Elevator A.I.\nv2.2.2\nby SKiRich",
+	'description', "Elevator A.I.\nv2.4.0\nby SKiRich",
 	'image', "ElevatorAILogo.png",
-	'last_changes', "v2.2.2 Dec 15th, 2018",
+	'last_changes', "v2.2.2 April 24th, 2019",
 	'ignore_files', {
 		"*.git/*",
 		"*.svn/*",
@@ -14,12 +14,40 @@ return PlaceObj('ModDef', {
 	'pops_desktop_uuid', "61f8d83b-4ba6-411d-be5a-792fe6d95b77",
 	'pops_any_uuid', "06f67c2e-9c98-4985-8a05-67558e42296e",
 	'author', "SkiRich",
-	'version', 27,
+	'version', 29,
 	'lua_revision', 233360,
-	'saved_with_revision', 240674,
-	'code', {"Code/EAI_1Init.lua","Code/EAI_2Panels.lua"},
-	'loctables', {{filename = "Locale/FR-fr.csv",language = "French"},{filename = "Locale/PL-pl.csv",language = "Polish"},{filename = "Locale/DE-de.csv",language = "German"},{filename = "Locale/BG-bg.csv",language = "Bulgarian"},{filename = "Locale/ES-es.csv",language = "Spanish"},{filename = "Locale/RU-ru.csv",language = "Russian"}},
-	'saved', 1548451146,
+	'saved_with_revision', 244275,
+	'code', {
+		"Code/EAI_1Init.lua",
+		"Code/EAI_2Panels.lua",
+	},
+	'loctables', {
+		{
+			filename = "Locale/FR-fr.csv",
+			language = "French",
+		},
+		{
+			filename = "Locale/PL-pl.csv",
+			language = "Polish",
+		},
+		{
+			filename = "Locale/DE-de.csv",
+			language = "German",
+		},
+		{
+			filename = "Locale/BG-bg.csv",
+			language = "Bulgarian",
+		},
+		{
+			filename = "Locale/ES-es.csv",
+			language = "Spanish",
+		},
+		{
+			filename = "Locale/RU-ru.csv",
+			language = "Russian",
+		},
+	},
+	'saved', 1557985363,
 	'screenshot1', "Images/ElevatorAILogo.png",
 	'screenshot2', "Images/EAIdisabled.png",
 	'screenshot3', "Images/EAIip1.png",
